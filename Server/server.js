7a98b5a0ed3259e0b6112f61659d46de4c8e47bc
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-05-15 14:41:23 -0300

    atualizando index.html e server.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const exp = require('constants')
-const express = require('express')
+const express = require('express');
+const { getDefaultAutoSelectFamily } = require('net');
 const path = require('path')
 
 
@@ -14,84 +15,34 @@ app.set('view engine', 'html');
 
 
 app.use('/', (req, res) => {
-    res.render('index.html')
+    var path = req._parsedOriginalUrl.pathname 
+    switch(path){
+        case '/':
+            res.render('index.html')
+            break;
+        case '/lobbys':
+            res.render('lobbys.html')
+            break;
+        case '/game':
+                res.render('jogo.html')
+                break;
+        default:
+            res.render('index.html')
+            break;
+    }
 })
 
-let connections = [];
+
 let users = [];
+
 let lobbys = [];
 
 io.on('connection', socket => {
     socket.emit('serverOn', socket.id)
-    socket.on('OldClient', function(data){
-        console.log(connections)
-        console.log('----------------')
-        var i = 0
-        if(connections.length === 0){
-            console.log('Nao tem nenhum user aqui')
-            connections.push(data)
-        }else{
-            while(i < connections.length){
-                if(data === connections[i]){
-                    console.log(`TEM ELE AQUI ${data} === ${connections[i]}`)
-                    break
-                }
-                if(i+1 === connections.length){
-                    console.log(`USER: ${data} NAO ESTA AQUI`)
-                    connections.push(data)
-                    break
-                }
-                i++
-            }
-        }
-    })
-    socket.on('NewClient', function(data){
-        console.log(`New User: ${data}`)
-        connections.push(data)
-        socket.emit('ClientConfirm', {
-            Client: data,
-            msg: 'Confirm Client database'
-        })
-    })
+   
 
-    socket.on('userRegister', function(data){
-        console.log('Novo user')
-        var i = 0
-       if(users.length === 0){
-            users.push(data)
-       }else{
-        while(i < users.length){
-            if(data.id === users[i].id){
-                console.log('USER JA SALVO')
-                break
-            }
-            if(i+1 === users.length){
-                console.log('Salvando novo user')
-                users.push(data)
-                break
-            }
-            i++
-        }
-       }
-    })
-
-    var i = 0
-     while(i < users.length){
-        if(socket.id === users[i].id){
-            console.log('USER JA SALVO')
-            socket.emit('OldUserver', users[i])
-            break
-        }  
-        i++
-    }
-    
-
-    
-
-
-
-    socket.on('connectionsList', function(data){
-        socket.emit('connectionsListResult', connections)
+    socket.on('searchLobbys', function(data){
+        console.log(data)
     })
     socket.on('clientOff', function(data){
         console.log(data)
